9742a68c516e017f20c37f74fabc84b70147c379
Sei Kim <vanism7557@gmail.com>
2023-01-05 20:11:22 +0900

    feat. 계산기 view 만들기

---
+++ cs193p-calculator-uikit/cs193p-calculator-uikit/Model/CalculatorBrain.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorBrain.swift
+//  cs193p-calculator-uikit
+//
+//  Created by sei_dev on 1/5/23.
+//
+
+import Foundation
---
+++ cs193p-calculator-uikit/cs193p-calculator-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cs193p-calculator-uikit
+//
+//  Created by sei_dev on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
