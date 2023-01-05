6e04df5f83b01b19d7f9b3d64889e4ee0ff2070f
Sei Kim <vanism7557@gmail.com>
2023-01-05 20:59:18 +0900

    feat. view, 컨트롤러 연결, 모델 조금 구현

--- cs193p-calculator-uikit/cs193p-calculator-uikit/Model/CalculatorBrain.swift
+++ cs193p-calculator-uikit/cs193p-calculator-uikit/Model/CalculatorBrain.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+    
+    var operations: Dictionary<String, Operations> = [
+        "𝝿": .constants(Double.pi)
+    ]
+    
+    enum Operations {
+        case constants(Double)
+        case unaryOperation((Double)->(Double))
+        case binaryOperation((Double, Double) -> (Double))
+        case Equals
+    }
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+
+    //    func performOperation(symbol: Operations) {
+    func performOperation(symbol: String) -> Double {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constants(let x):
+                return x
+            default:
+                return 0.0
+            }
+        }
+    }
+    
+}
--- cs193p-calculator-uikit/cs193p-calculator-uikit/ViewController.swift
+++ cs193p-calculator-uikit/cs193p-calculator-uikit/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak private var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping: Bool = false
+    private var resultContainsDot: Bool = false
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text ?? "0") ?? 0
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        if let textCurrentInDisplay = display.text,
+           let pressedDigit = sender.titleLabel?.text {
+            switch pressedDigit {
+            case ".":
+                if false == resultContainsDot {
+                    display.text = textCurrentInDisplay + pressedDigit
+                    resultContainsDot = true
+                    userIsInTheMiddleOfTyping = true
+                }
+            default:
+                if userIsInTheMiddleOfTyping {
+                    display.text = textCurrentInDisplay + pressedDigit
+                } else {
+                    display.text = pressedDigit
+                    userIsInTheMiddleOfTyping = true
+                }
+            }
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.titleLabel {
+            brain.performOperation(symbol: mathematicalSymbol)
+        }
+    }
+    
 }
 
